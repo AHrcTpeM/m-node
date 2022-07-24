216504e45abca4f21bc3407546c237426c61c879
AHrcTpeM <igorpopkov86@gmail.com>
2022-07-24 19:19:59 +0300

    Add express

---
+++ level2/lesson3/express/express.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const app = express();
+
+const port = 3000
+
+const urlencodedParser = express.urlencoded({extended: false});
+
+app.get('/', (req, res) => {
+  res.status(200).type('text/plain')
+  res.send('Sh++\nHey, our server is up, hooray! And you were able to connect to it')
+})
+
+app.use((req, res, next) => {
+  res.status(404).type('text/plain')
+  res.send('Not found')
+})
+
+app.listen(port, function () {
+  console.log(`Server listens port: ${port}`)
+})
