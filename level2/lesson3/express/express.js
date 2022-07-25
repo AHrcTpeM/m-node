36d3179b2bb63ea8f5dc80b562f34ca26b8dfd2f
AHrcTpeM <igorpopkov86@gmail.com>
2022-07-25 10:37:31 +0300

    Add docker file

--- level2/lesson3/express/express.js
+++ level2/lesson3/express/express.js
@@ -1,16 +1,18 @@
 const express = require('express');
 const app = express();
 
-const port = 3000
-
-const urlencodedParser = express.urlencoded({extended: false});
+const port = 3000;
 
 app.get('/', (req, res) => {
   let log = `Request time: ${new Date().toUTCString()}\nUser IP: ${req.socket.remoteAddress}:${req.socket.remotePort}`;
   console.log(log);
 
   res.status(200).type('text/plain')
-  res.send('Sh++\nHey, our server is up, hooray! And you were able to connect to it')
+  res.send(`
+  Sh++
+  Hi traveler, I'm a server, nice to meet you.
+  I was deployed from a docker-container to a remote AWS-server in Frankfurt. Cool, right?\n
+  Pss, our server is up, hooray! And you were able to connect to it.`)
 })
 
 app.use((req, res, next) => {
